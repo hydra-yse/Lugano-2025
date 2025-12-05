39edfe5995e767429780cdcd9af089e54297aada
yse <hydra_yse@proton.me>
2025-12-06 00:57:00 +0100

    feat[l1]: add profiles modal

--- Lecture-1/demo/src/components/Modal.tsx
+++ Lecture-1/demo/src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export interface ModalProps {
   children?: React.ReactNode,
@@ -7,6 +7,8 @@ export interface ModalProps {
 }
 
 export const Modal = ({ children, close }: ModalProps) => {
+  const bgRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     const escClosesModal = (e: KeyboardEvent) => {
       if (e.key == 'Escape') close();
@@ -17,9 +19,17 @@ export const Modal = ({ children, close }: ModalProps) => {
     }
   }, [])
 
+  const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === bgRef.current) close();
+  }
+
   return (
-    <div className="modal-wrapper fixed h-full w-full z-99 flex items-center justify-center">
-      <div className="relative rounded-md min-h-1/2 min-w-1/2 bg-black border-2 border-primary text-primary text-center">
+    <div
+      ref={bgRef}
+      onClick={handleClose}
+      className="modal-wrapper fixed h-full w-full z-99 flex items-center justify-center"
+    >
+      <div className="relative rounded-md min-h-1/3 min-w-1/2 bg-black border-2 border-primary text-primary text-center">
         <XMarkIcon className="absolute cursor-pointer right-2 top-2 text-primary" height={16} onClick={close} />
         <div className="p-4">
           {children}
--- Lecture-1/demo/src/components/sidebar/modals/Profiles.tsx
+++ Lecture-1/demo/src/components/sidebar/modals/Profiles.tsx
@@ -1,10 +1,39 @@
+import type { BindingLiquidSdk } from "@breeztech/breez-sdk-liquid/web"
 import { Modal, type ModalProps } from "@components/Modal"
+import { P1_SDK, P2_SDK } from "BreezSdk"
+
+interface ProfilesCardProps {
+  sdk: BindingLiquidSdk | null,
+  playerName: string,
+}
+
+export const ProfileCard = ({ sdk, playerName }: ProfilesCardProps) => {
+  if (!sdk) return 'Loading...';
+  return (
+    <div className="flex flex-col border-1 rounded-lg border-primary p-5 gap-1 w-full">
+      <div className="flex self-center rounded-full bg-primary w-[90px] p-4">
+        <img className="h-[55px] mx-auto" src='https://cdn-icons-png.flaticon.com/512/1077/1077114.png' />
+      </div>
+
+      <h2 className="text-lg font-bold">{playerName}</h2>
+      <div className="text-sm">
+        <span>Games won: {0}</span><br />
+        <span>Money lost: {0}</span><br />
+        <span>Current balance: {0}</span><br />
+      </div>
+    </div>
+  )
+}
 
 export const ProfilesModal = (props: ModalProps) => {
   // TODO: Add your code here
   return (
     <Modal {...props}>
-      <h1 className="text-2xl">Profiles</h1>
+      <h1 className="text-2xl mb-1">Profiles</h1>
+      <div className="flex flex-col w-full px-4 py-2 gap-3">
+        <ProfileCard playerName='P1' sdk={P1_SDK} />
+        <ProfileCard playerName='P2' sdk={P2_SDK} />
+      </div>
     </Modal>
   )
 }
